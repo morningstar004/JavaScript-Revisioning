2e9167e40ccc9247ef621077406012b8d0f3216f
morningstar004 <pranjalkumar000007@gmail.com>
2026-01-21 10:20:56 +0530

    compleated project 7

--- Projects/project7/script.js
+++ Projects/project7/script.js
@@ -1,1 +1,20 @@
-const url = 'https://api.thecatapi.com/v1/images/search';
+const catUrl = "https://api.thecatapi.com/v1/images/search";
+
+const catButton = document.querySelector(".btn");
+const catBox = document.querySelector(".container");
+const image = document.createElement("img");
+
+catButton.addEventListener("click", () => {
+  const catGallery = new XMLHttpRequest();
+  catGallery.open("GET", catUrl);
+  catGallery.send();
+  
+  catGallery.onreadystatechange = function () {
+    if (catGallery.readyState === 3) {
+      const data = JSON.parse(this.responseText);
+      image.setAttribute("src", data[0].url);
+      catBox.appendChild(image);
+      console.log(data[0].url);
+    }
+  };
+});
